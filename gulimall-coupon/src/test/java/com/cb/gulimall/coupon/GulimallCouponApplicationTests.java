5495d85be1480d603a22b6f250234b56e5cefe1d
chenbiao <994700146@qq.com>
2021-09-15 01:59:37 +0800

    项目初始化结构创建

---
+++ gulimall-coupon/src/test/java/com/cb/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cb.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

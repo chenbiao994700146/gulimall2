7dbac686cd11031e5d0bb2931b55c5b66188615c
chenbiao <994700146@qq.com>
2022-01-21 11:08:27 +0800

    zhi

---
+++ gulimall-third-party/src/main/java/com/cb/gulimall/thirdparty/controller/SmsSendController.java
@@ -0,0 +1,31 @@
+package com.cb.gulimall.thirdparty.controller;
+
+import com.cb.common.utils.R;
+import com.cb.gulimall.thirdparty.component.SmsComponent;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("/sms")
+public class SmsSendController {
+
+    @Resource
+    SmsComponent smsComponent;
+
+
+    /**
+     * 提供给别的服务进行调用
+     * @param phone
+     * @param code
+     * @return
+     */
+    @GetMapping("/sendCode")
+    public R sendCode(@RequestParam("phone") String phone, @RequestParam("code") String code){
+        smsComponent.sendSmsCode(phone,code);
+        return R.ok();
+    }
+}
---
+++ gulimall-third-party/src/test/java/com/cb/gulimall/thirdparty/GulimallThirdPartyApplicationTests.java
@@ -0,0 +1,98 @@
+package com.cb.gulimall.thirdparty;
+
+//import org.junit.jupiter.api.Test;
+import com.aliyun.oss.OSS;
+import com.cb.gulimall.thirdparty.component.SmsComponent;
+import com.cb.gulimall.thirdparty.util.HttpUtils;
+import org.apache.http.HttpResponse;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GulimallThirdPartyApplicationTests {
+
+    @Test
+  public  void contextLoads() {
+    }
+
+    @Resource
+    OSS ossClient;
+
+    @Resource
+    SmsComponent smsComponent;
+
+    @Test
+    public void cbSms(){
+        smsComponent.sendSmsCode("15973293443","520");
+    }
+
+    @Test
+    public void sendSms(){
+        String host = "https://dfsns.market.alicloudapi.com";
+        String path = "/data/send_sms";
+        String method = "POST";
+        String appcode = "1b13faadb41e4d72a387a990e272ce11";
+        Map<String, String> headers = new HashMap<String, String>();
+        //最后在header中的格式(中间是英文空格)为Authorization:APPCODE 83359fd73fe94948385f570e3c139105
+        headers.put("Authorization", "APPCODE " + appcode);
+        //根据API的要求，定义相对应的Content-Type
+        headers.put("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+        Map<String, String> querys = new HashMap<String, String>();
+        Map<String, String> bodys = new HashMap<String, String>();
+        bodys.put("content", "code:1234");
+        bodys.put("phone_number", "17673632884");
+        bodys.put("template_id", "TPL_0000");
+
+
+        try {
+            /**
+             * 重要提示如下:
+             * HttpUtils请从
+             * https://github.com/aliyun/api-gateway-demo-sign-java/blob/master/src/main/java/com/aliyun/api/gateway/demo/util/HttpUtils.java
+             * 下载
+             *
+             * 相应的依赖请参照
+             * https://github.com/aliyun/api-gateway-demo-sign-java/blob/master/pom.xml
+             */
+            HttpResponse response = HttpUtils.doPost(host, path, method, headers, querys, bodys);
+            System.out.println(response.toString());
+            //获取response的body
+            //System.out.println(EntityUtils.toString(response.getEntity()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testUpload1() throws FileNotFoundException {
+//        // yourEndpoint填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
+//        String endpoint = "oss-cn-shanghai.aliyuncs.com";
+//        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
+//        String accessKeyId = "LTAI5tCzDzR8LEpYVXBvZTAj";
+//        String accessKeySecret = "D8jGlxKBeEm71NGw4543PDnVNDzo7J";
+
+        // 创建OSSClient实例。
+        //OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+        // 填写本地文件的完整路径。如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件流。
+        InputStream inputStream = new FileInputStream("/Users/chenbiao/Desktop/图片/WechatIMG11411.jpeg");
+        // 依次填写Bucket名称（例如examplebucket）和Object完整路径（例如exampledir/exampleobject.txt）。Object完整路径中不能包含Bucket名称。
+        ossClient.putObject("gulimallcb", "6.jpg", inputStream);
+
+        // 关闭OSSClient。
+        ossClient.shutdown();
+        System.out.println("上次成功。。。");
+
+    }
+
+}
